f8196c1b12ba7d5335b5cff51213867b947bd881
alexander <alexander.flores0920@gmail.com>
2023-06-19 23:34:36 -0700

    Simple code for reading temperature/pH. Not tested, not calibrated.
    The file sensors.ino reads the analog input (10 bit number == 1024) from the sensors,
    then, turns into a voltage value. Finally, the voltage is turn into a usefull value,
    depending on the sensor, and is printed to the user.
    
    Note: The formula for converting a voltage to a usefull value is dependent on the sensor
    you are using. Check the vendors datasheet for more information about the sensor
    Note2: The temperature and pH sensor are from atlas-scientific.
    
    The file sketch.yaml stores configuration information for the Arduino CLI. This file is
    not required but makes the Arduino CLI easier to use.

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,6 +1,72 @@
+#include <Arduino.h>
+
+#define TEMP_OUTPUT_PIN 8
+#define TEMP_INPUT_PIN A5
+
+#define PH_OUTPUT_PIN 7
+#define PH_INPUT_PIN A4
 
 void setup() {
+	
+	Serial.begin(9600);
+	/* wait for Serial port to open for data transfer */
+	while (!Serial) {;}
+	
+	pinMode(TEMP_OUTPUT_PIN, OUTPUT);
+	pinMode(TEMP_INPUT_PIN, INPUT);
+	
+	//pinMode(PH_OUTPUT_PIN, OUTPUT);
+	//pinMode(PH_INPUT_PIN, INPUT);
 }
 
 void loop() {
+	
+	PT1000Sensor();
+	//pHSensor();
+	
+}
+
+float sensorVoltage(uint8_t output_pin, uint8_t input_pin) {
+	
+	/* use 5v pin for a higher current */
+	digitalWrite(output_pin, HIGH); 
+	delay(1000);
+
+	/* final_voltage = final_analog_value / max_analog_value * initial_voltage */
+	float V = analogRead(input_pin) / 1024.0 * 5.0;
+
+	digitalWrite(output_pin, LOW);
+
+	return V;
+
+}
+
+void PT1000Sensor() {
+ 
+	// might need to charge up for 8 seconds? 
+	// documentation says Meter will output 0V-3V, so 3.0? So anything >3.0 is not supported.
+	float V = sensorVoltage(TEMP_OUTPUT_PIN, TEMP_INPUT_PIN); // might want too loop so i can get an average
+
+	/* Voltage to temperature (C) */
+	float tempC = (V - 1.058) / .009;
+	/* Temperature C to F */
+	//float tempF = tempC * 1.8 + 32.0;
+
+	/* Use either tempC or tempF, not both */
+	Serial.print("PT ");
+	Serial.println(tempC);
+	//Serial.println(tempF);
+
+}
+
+void pHSensor() {
+	
+	float V = sensorVoltage(PH_OUTPUT_PIN, PH_INPUT_PIN);
+
+	/* Voltage to pH value */
+	float pH = (-5.6548 * V) + 15.509;
+
+	Serial.print("PH ");
+	Serial.println(pH);
+
 }
